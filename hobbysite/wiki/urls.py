5a5d6e657c6e42a3983a7e1746e6d1ae553e0af4
andrewvillorente1 <157769842+andrewvillorente1@users.noreply.github.com>
2024-03-20 23:49:14 +0800

    Delete hobbysite directory

--- hobbysite/wiki/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ArticleListView, ArticleDetailView
-
-urlpatterns = [
-	path('wiki/articles/', ArticleListView.as_view(),name='articles'),
-	path('wiki/article/<int:pk>', ArticleDetailView.as_view(), name='article'),
-]
-
-app_name = 'wiki'
