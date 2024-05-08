412c543d759c13b35036052551c3f3779e90ac00
miguidm <157345126+miguidm@users.noreply.github.com>
2024-05-08 23:35:30 +0800

    update

--- hobbysite/user_management/admin.py
+++ hobbysite/user_management/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import Profile
-
-
-class ProfileAdmin(admin.ModelAdmin):
-    model = Profile 
-
-admin.site.register(Profile)
+# Register your models here.
--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import update_profile
+from .views import profile_update, user_create, profile_create, dashboard_view
 
 app_name = 'user_management'
 
 urlpatterns = [
-    path('profile/', update_profile, name='update_profile'),
+    path('profile/', profile_update, name='update_profile'),
+    path('register/', user_create, name='register'),
+    path('profile/create/', profile_create, name='profile_create'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+
+
 ]
