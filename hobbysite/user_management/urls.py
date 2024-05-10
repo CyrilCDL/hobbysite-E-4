e51e1e0964ad2e99b68c5225a5836c9b5968bdff
miguidm <157345126+miguidm@users.noreply.github.com>
2024-05-10 22:11:28 +0800

    update urls and views

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import profile_update, user_create, profile_create, dashboard_view
+from .views import profile_update, user_create, profile_create, DashboardView
 
 app_name = 'user_management'
 
@@ -7,7 +7,6 @@ urlpatterns = [
     path('profile/', profile_update, name='update_profile'),
     path('register/', user_create, name='register'),
     path('profile/create/', profile_create, name='profile_create'),
-    path('dashboard/', dashboard_view, name='dashboard'),
 
 
 ]
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
+from collections import defaultdict
+
 from .forms import ProfileForm, CreateProfileForm, UserRegistrationForm
-from .models import Profile
-from django.contrib.auth.models import User
 from commissions.models import Commission
+from merchstore.models import Product, Transaction
 
 
 @login_required
@@ -34,26 +38,11 @@ def user_create(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('/homepage')
+            try:
+                form.save()
+                return redirect('/homepage')
+            except forms.ValidationError as e:
+                form.add_error(None, e)  # Add form-wide error
     else:
         form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
-
-
-@login_required
-def dashboard_view(request):
-    profile = request.user.profile
-    username = request.user.username
-
-    my_posted_commissions = Commission.objects.filter(author=profile)
-    my_applied_commissions = Commission.objects.filter(
-        job__jobapplication__applicant=profile).distinct()
-
-    context = {
-        'profile': profile,
-        'username': username,
-        'my_posted_commissions': my_posted_commissions,
-        'my_applied_commissions': my_applied_commissions
-    }
-    return render(request, 'dashboard.html', context)
