dbff258d5d99b86c0a473346ddaf8d3ff1e556ac
miguidm <157345126+miguidm@users.noreply.github.com>
2024-05-12 10:13:35 +0800

    wrong file uploaded

--- hobbysite/user_management/forms.py
+++ hobbysite/user_management/forms.py
@@ -20,7 +20,24 @@ class CreateProfileForm(forms.ModelForm):
 
 class UserRegistrationForm(forms.ModelForm):
     email = forms.EmailField()
+    display_name = forms.CharField(max_length=150, required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['display_name', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        display_name = self.cleaned_data.get('display_name')
+        if display_name:
+            if User.objects.filter(username=display_name).exists():
+                raise forms.ValidationError(
+                    "Username already exists. Please choose a different one.")
+            else:
+                user.username = display_name
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            profile = Profile.objects.create(
+                user=user, display_name=display_name)
+        return user
