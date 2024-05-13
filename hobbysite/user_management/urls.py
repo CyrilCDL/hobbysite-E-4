d44e8336d77c323abd875dd5f6cbb44aee83163c
miguidm <157345126+miguidm@users.noreply.github.com>
2024-05-13 13:06:57 +0800

    wrong file uploaded

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import profile_update, user_create, profile_create, DashboardView
+
+from .views import profile_update, user_create, profile_create
 
 app_name = 'user_management'
 
@@ -7,6 +8,4 @@ urlpatterns = [
     path('profile/', profile_update, name='update_profile'),
     path('register/', user_create, name='register'),
     path('profile/create/', profile_create, name='profile_create'),
-
-
 ]
